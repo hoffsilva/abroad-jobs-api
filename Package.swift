3839675dc7ad58bdbb2b8ed01a773886c29f00b3
Lucas dos Santos <lucas.leite@protonmail.com>
2018-10-16 15:03:50 -0300

    Removing database dependency

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-
         // SwiftSoup: Pure Swift HTML Parser, with best of DOM, CSS, and jquery (Supports Linux, iOS, Mac, tvOS, watchOS)
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "SwiftSoup"]),
+        .target(name: "App", dependencies: ["Vapor", "SwiftSoup"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
