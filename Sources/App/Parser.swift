85c84b9db466e2900ec834df8307ad052ad2f756
Hoff Silva <hoff.henry@gmail.com>
2019-02-17 20:57:00 +0000

    Added remotely awesome jobs

--- Sources/App/Parser.swift
+++ Sources/App/Parser.swift
@@ -1,5 +1,8 @@
-import Vapor
+import Foundation
 import SwiftSoup
+<<<<<<< HEAD
+import Vapor
+=======
 import Foundation
 
 enum URLS: String {
@@ -9,27 +12,40 @@ enum URLS: String {
     case landingJobs = "https://landing.jobs/jobs/search.json"
     case landingJobsSearch = "https://landing.jobs/jobs/search.json?page="
 }
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
 
 final class Parser {
     let client: Client
-    
+    var currentPageRemotelyAwesome = 1
+
     init(client: Client) {
         self.client = client
     }
-    
+
     public func getJobs(_ worker: Worker) throws -> Future<[[Job]]> {
         var futureJobs: [Future<[Job]>] = []
-        
         futureJobs.append(try getJobsFromRemoteOK(on: worker))
         futureJobs.append(try getJobsFromLandingJobs(on: worker))
         futureJobs.append(try getJobsFromCryptoJobs(on: worker))
         futureJobs.append(try getJobsFromVanhack(on: worker))
+<<<<<<< HEAD
+        for _ in 1 ... 57 {
+            futureJobs.append(try getJobsRemotelyAwesome(on: worker))
+            currentPageRemotelyAwesome += 1
+        }
+        return futureJobs.flatten(on: worker)
+    }
+
+    private func getJobsFromRemoteOK(on _: Worker) throws -> Future<[Job]> {
+        guard let url = URL(string: Constants.remoteOkURL) else {
+=======
         
         return futureJobs.flatten(on: worker)
     }
     
     private func getJobsFromRemoteOK(on worker: Worker) throws -> Future<[Job]> {
         guard let url = URL(string: URLS.remoteOk.rawValue) else {
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
             throw Abort(.internalServerError)
         }
         return client.get(url).map { response in
@@ -37,7 +53,7 @@ final class Parser {
             let document = try SwiftSoup.parse(html)
             let tbody = try document.select("tbody tr").array()
             let pairs: [(Element, Element?)] = stride(from: 0, to: tbody.count, by: 2).map {
-                (tbody[$0], $0 < tbody.count-1 ? tbody[$0.advanced(by: 1)] : nil)
+                (tbody[$0], $0 < tbody.count - 1 ? tbody[$0.advanced(by: 1)] : nil)
             }
             var jobs = [Job]()
             for (trData, trDescription) in pairs {
@@ -52,16 +68,43 @@ final class Parser {
                 let tags = try arrayData[3].select("h3").array().map { try $0.text() }
                 var jobDescription = try trDescription?.text().components(separatedBy: " See more jobs at").first ?? ""
                 jobDescription = jobDescription.replacingOccurrences(of: "{linebreak}", with: "\n")
-                
-                let job = Job(jobTitle: jobTitle, companyLogoURL: companyLogoURL, companyName: companyName, jobDescription: jobDescription, applyURL: applyURL, tags: tags, source: "remote-ok")
+                let job = Job(jobTitle: jobTitle, companyLogoURL: companyLogoURL, companyName: companyName, jobDescription: jobDescription, applyURL: applyURL, tags: tags, source: Constants.remoteOkSource)
                 jobs.append(job)
             }
             return jobs
         }
     }
+
+    private func getJobsRemotelyAwesome(on _: Worker) throws -> Future<[Job]> {
+        guard let url = URL(string: Constants.remotelyAwesomeJobsURL + String(currentPageRemotelyAwesome)) else {
+            throw Abort(.internalServerError)
+        }
+        return client.get(url).map { response in
+            let html = response.http.body.description
+            let document = try SwiftSoup.parse(html)
+            let htmlJobsList = try document.select("div.jobs-container li").array()
+            var jobs = [Job]()
+            for htmlJob in htmlJobsList {
+                let jobTitle = try htmlJob.select("h2 a").text()
+                let companyName = try htmlJob.select("h2 span span").text()
+                let applyURL = try Constants.remotelyAwesomeJobDetailURL + htmlJob.select("h2 a").attr("href")
+                let tags = try htmlJob.select("meta").array()[1].attr("content").components(separatedBy: ",")
+                let jobDescription = ""
+                let job = Job(jobTitle: jobTitle, companyLogoURL: "", companyName: companyName, jobDescription: jobDescription, applyURL: applyURL, tags: tags, source: Constants.remotelyAwesomeJobsSource)
+                jobs.append(job)
+            }
+            return jobs
+        }
+    }
+<<<<<<< HEAD
+
+    private func getJobsFromVanhack(on _: Worker) throws -> Future<[Job]> {
+        guard let url = URL(string: Constants.vanhackJobsURL) else {
+=======
     
     private func getJobsFromVanhack(on worker: Worker) throws -> Future<[Job]> {
         guard let url = URL(string: URLS.vanhackJobs.rawValue) else {
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
             throw Abort(.internalServerError)
         }
         var jobs = [Job]()
@@ -76,49 +119,65 @@ final class Parser {
             return jobs
         }
     }
+<<<<<<< HEAD
+
+    private func getJobsFromLandingJobs(on worker: Worker) throws -> Future<[Job]> {
+        guard let url = URL(string: Constants.landingJobsURL) else {
+=======
     
     private func getJobsFromLandingJobs(on worker: Worker) throws -> Future<[Job]> {
         guard let url = URL(string: URLS.landingJobs.rawValue) else {
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
             return worker.future([])
         }
-        
+
         return client.get(url).flatMap { response in
             guard let data = response.http.body.data else {
                 return worker.future([])
             }
-            
+
             var jobs = [Future<[Job]>]()
             let landingJobsData = try JSONDecoder().decode(LandingJobsData.self, from: data)
             let firstPageJobs = landingJobsData.offers.map { Job($0) }
             jobs.append(worker.future(firstPageJobs))
+<<<<<<< HEAD
+
+            for page in 2 ... landingJobsData.numberOfPages {
+                guard let url = URL(string: Constants.landingJobsSearchURL + String(page)) else {
+=======
             
             for page in (2...landingJobsData.numberOfPages) {
                 guard let url = URL(string: URLS.landingJobsSearch.rawValue + String(page)) else {
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
                     break
                 }
-                
+
                 jobs.append(self.client.get(url).map { response in
                     guard let data = response.http.body.data else {
                         return []
                     }
-                    
+
                     let landingJobsData = try JSONDecoder().decode(LandingJobsData.self, from: data)
                     let jobs = landingJobsData.offers.map { Job($0) }
                     return jobs
                 })
             }
-            
+
             return jobs.flatten(on: worker).map { futureJobs in
-                return Array(futureJobs.joined())
+                Array(futureJobs.joined())
             }
         }
     }
-    
+
     private func getJobsFromCryptoJobs(on worker: Worker) throws -> Future<[Job]> {
+<<<<<<< HEAD
+        guard let url = URL(string: Constants.cryptoJobsURL) else {
+=======
         guard let url = URL(string: URLS.cryptoJobs.rawValue) else {
+>>>>>>> 9ec628e973ba2b3c54a9be33ea636c4fe17a007c
             return worker.future([])
         }
-        
+
         var jobs = [Job]()
         return client.get(url).map { response in
             guard let data = response.http.body.data else {
